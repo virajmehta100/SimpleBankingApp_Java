05e8b0b8f5669c8a417360f72ec83520f182aa55
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-25 22:19:31 +0530

    new features

--- src/com/Package/Bank.java
+++ src/com/Package/Bank.java
@@ -25,4 +25,21 @@ public class Bank {
         }
     }
 
+    void getPreviousTransaction(){
+        if(previousTransaction > 0){
+            System.out.println("Deposited: " + previousTransaction);
+        }else if (previousTransaction < 0){
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        }else{
+            System.out.println("No transaction occurred");
+        }
+    }
+
+    void calculateInterest(int years){
+        double interestRate = .0185;
+        double newBalance = (balance + interestRate * years) + balance;
+        System.out.println("The current interest rate is " + (100 * interestRate));
+        System.out.println("After " + years + " years, your balance will be: " + newBalance);
+    }
+
 }
