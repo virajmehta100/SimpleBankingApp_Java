33db38adc97a61849c0013dc139eba82f9d28f52
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-25 22:37:30 +0530

    Main application configuration

--- src/com/Package/Main.java
+++ src/com/Package/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Bank Viraj = new Bank("Viraj Mehta","A000262");
+        Viraj.showMenu();
     }
 }
